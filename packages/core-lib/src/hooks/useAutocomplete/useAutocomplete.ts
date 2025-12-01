4a9068af877f3ced14fa590fe0f01bf023f38173
Gopala Agrawal <agrawalgopala@gmail.com>
2025-12-01 14:35:52 +0530

    chore: adding min length

--- packages/core-lib/src/hooks/useAutocomplete/useAutocomplete.ts
+++ packages/core-lib/src/hooks/useAutocomplete/useAutocomplete.ts
@@ -1,17 +1,33 @@
-import { UseAutocompleteOptions } from './types';
 import { useState } from 'react';
+import type { UseAutocompleteOptions } from './types';
 
 export const useAutocomplete = <T>(options: UseAutocompleteOptions<T>) => {
-  const { fetchResults } = options;
+  const { fetchResults, minLength = 1 } = options;
 
+  const [value, setValue] = useState('');
   const [results, setResults] = useState<T[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const onChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
     const query = e.target.value;
+    setValue(query);
 
-    const results = await fetchResults(query);
-    setResults(results);
+    if (query.length < minLength) {
+      setResults([]);
+      return;
+    }
+
+    setIsLoading(true);
+    const newResults = await fetchResults(query);
+    setResults(newResults);
+    setIsLoading(false);
   };
 
-  return { results, onChange };
+  return {
+    value,
+    results,
+    isLoading,
+    onChange,
+    setValue,
+  };
 };
